32ffa000b0f4ad9bf5565db678ff529649d9270c
somnathacn <64811320+somnathacn@users.noreply.github.com>
2020-05-13 12:43:12 -0400

    Added exercises

--- com.conferencedemo-master/src/main/java/com/conferencedemo/Application.java
+++ com.conferencedemo-master/src/main/java/com/conferencedemo/Application.java
@@ -2,7 +2,12 @@ package com.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
--- zuulServiceSA/src/main/java/com/example/demo/ZuulServiceSaApplication.java
+++ zuulServiceSA/src/main/java/com/example/demo/ZuulServiceSaApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServiceSaApplication {
